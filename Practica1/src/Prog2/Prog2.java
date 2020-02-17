ffc892953a3e4f076235bdc61fadaf04222991ee
davidroma27 <davidroma27@gmail.com>
2020-02-17 23:49:37 +0100

    Añadidos comentarios al Prog2.java

--- Practica1/src/Prog2/Prog2.java
+++ Practica1/src/Prog2/Prog2.java
@@ -1,17 +1,19 @@
 package Prog2;
 
+//En este ejemplo arrancamos un Thread desde fuera de la clase main donde se crea el hilo principal
 public class Prog2 extends Thread {
     public Prog2(String name) {
         super(name);
     }
     @Override
-    public void run() {
+    public void run() { //Metodo que muestra el hilo que se está ejecutando
+                        //Este metodo se ejecuta una vez que la maquina virtual de java (JVM) arranca el hilo
         System.out.println("Executing thread "
                 +Thread.currentThread().getName());
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Prog2 myThread = new Prog2("myThread");
-        myThread.start();
+        Prog2 myThread = new Prog2("myThread"); //Creating the main Thread in main method
+        myThread.start(); //starting the thread
     }
 }
